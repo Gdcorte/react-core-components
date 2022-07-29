6d678ec5ad820ef16fc1d0378771aabd12525743
Gustavo <gustavodacorte@gmail.com>
2022-07-29 20:57:57 +0900

    jumpstart on input

---
+++ src/components/Input/Password/index.tsx
@@ -0,0 +1,94 @@
+import {FunctionComponent, useCallback, useEffect, useMemo} from 'react'
+
+// import styles from '../../../../styles/Inputs.module.css'
+import { useState } from 'react'
+
+import { WrapperStyled } from './styles'
+
+import {SimpleInput} from '../'
+import { passwordStatus } from '../../../icons/password'
+
+import styled from 'styled-components'
+
+export const IconStyled = styled.div`
+    position: absolute;
+    margin-top: auto;
+    margin-bottom: auto;
+    margin-right: 8px;
+    right: -16px;
+    top: 0;
+    bottom: 0;
+    vertical-align: middle;
+    border-style: none;
+    cursor: pointer;
+
+    svg {
+        width: 20px;
+        height: 20px;
+
+        path:last-child{
+            fill: ${({theme})=> theme.primary};
+        }
+    }
+`
+
+export interface PasswordInterface { 
+    validInput?: boolean,
+    errorMessage?: string,
+    value: string,
+    onChange: CallableFunction,
+}
+
+const Password: FunctionComponent<PasswordInterface> = ({
+    validInput,
+    errorMessage,
+    ...props
+}) =>{
+    const [inputType, setinputType] = useState<'text'|'password'>('password')
+    const [lockStatus, setlockStatus] = useState(true)
+    
+    function toggleImgIcon(){
+        let newInputStatus: 'text'|'password' = 'password'
+
+        if (inputType =='password'){
+            newInputStatus = 'text'
+        }
+
+        setinputType(newInputStatus)
+        setlockStatus(!lockStatus)
+    }
+
+
+    const PasswordIcon = useMemo(
+      () => {
+        return lockStatus ? passwordStatus.hide : passwordStatus.show
+      },
+      [lockStatus],)
+    
+    
+
+    return(
+        <WrapperStyled>
+            <SimpleInput 
+                {...props}
+                validInput={validInput}
+                type={inputType}
+            />
+            
+            <IconStyled
+                className='cursor-pointer'
+                onClick={toggleImgIcon}
+            >
+                <PasswordIcon />
+            </IconStyled>
+        
+        </WrapperStyled>
+    )
+}
+
+Password.defaultProps = {
+    validInput: true,
+    errorMessage: '',
+}
+
+export default Password
---
+++ src/components/Input/Password/styles.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const WrapperStyled = styled.div`
+    display: flex;
+    position: relative;
+    width: 100%;
+`
+
+export const InputStyled = styled.input`
+    border-radius: 5px;
+    border: 1px solid ${({ theme }) => theme.background};
+    width: 100%;
+    padding-left: 8px;
+    color: ${({ theme }) =>  theme.text };
+
+    :focus-visible{
+        outline: none;
+        border:2px dotted ${({ theme }) => theme.background};
+    }
+`
+
+export const IconStyled = styled.div`
+    position: absolute;
+    margin-top: auto;
+    margin-bottom: auto;
+    margin-right: 8px;
+    right: -16px;
+    top: 0;
+    bottom: 0;
+    vertical-align: middle;
+    border-style: none;
+    cursor: pointer;
+`
