3bfb2eaf6c3e5202ffcc7adc99ddffa18b87d9f8
Gustavo Diniz da Corte <gustavodinizdacorte@Gustavos-MacBook-Pro.local>
2022-12-24 12:09:35 +0900

    improved key handling

--- src/components/Accordion/elements/Accordion.tsx
+++ src/components/Accordion/elements/Accordion.tsx
@@ -1,147 +1,162 @@
-import { FunctionComponent, useMemo, useState } from "react"
-import { default as  AccordionContent } from "./Content"
-import { SubAccordion, isSubAccordion, isAccordionItem, isEmptyTitle } from "../interface"
-import styled , {css} from 'styled-components'
-import { CarretDown } from "../../../icons"
-import { FontHelper } from "../../../themes"
+import { FunctionComponent, useMemo, useState } from "react";
+import { default as AccordionContent } from "./Content";
+import {
+  SubAccordion,
+  isSubAccordion,
+  isAccordionItem,
+  isEmptyTitle,
+} from "../interface";
+import styled, { css } from "styled-components";
+import { CarretDown } from "../../../icons";
+import { FontHelper } from "../../../themes";
 
 export interface AccordionMenuProps {
-    option: SubAccordion,
-    openMenu: string,
-    changeOpenMenu: CallableFunction,
-    parentLevel: number,
+  option: SubAccordion;
+  openMenu: string;
+  changeOpenMenu: CallableFunction;
+  parentLevel: number;
 }
 
 const AccordionContainer = styled.div`
-    color: ${({theme: {theme}})=> FontHelper.findBestContrast(theme.background.base, [theme.fonts.dark, theme.fonts.light])};
+  color: ${({ theme: { theme } }) =>
+    FontHelper.findBestContrast(theme.background.base, [
+      theme.fonts.dark,
+      theme.fonts.light,
+    ])};
+
+  svg {
+    fill: ${({ theme: { theme } }) =>
+      FontHelper.findBestContrast(theme.background.base, [
+        theme.fonts.dark,
+        theme.fonts.light,
+      ])};
+  }
+`;
+const AccordionTitle = styled.div`
+  padding: 6px 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  cursor: pointer;
+  border-radius: 5px;
+
+  :hover {
+    background-color: ${({ theme: { theme } }) => theme.background.shade1};
+    color: ${({ theme: { theme } }) => theme.primary.base};
 
     svg {
-        fill: ${({theme: {theme}})=> FontHelper.findBestContrast(theme.background.base, [theme.fonts.dark, theme.fonts.light])};
+      fill: ${({ theme: { theme } }) => theme.primary.base};
     }
-
-`
-const AccordionTitle = styled.div`
-    padding: 6px 10px;
-    display:flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-
-    cursor: pointer;
-    border-radius: 5px;
-
-    :hover{
-        background-color: ${({theme: {theme}})=> theme.background.shade1};
-        color: ${({theme: {theme}})=> theme.primary.base};
-
-        svg {
-            fill: ${({theme: {theme}})=> theme.primary.base};
-        }
-    }
-`
+  }
+`;
 
 const CarretCss = css`
-    margin-left: 4px;
-    width: 10px !important;
-    height: 10px !important;
-`
+  margin-left: 4px;
+  width: 10px !important;
+  height: 10px !important;
+`;
 
 const AccordionCarret = styled(CarretDown)`
-    ${CarretCss}
-`
+  ${CarretCss}
+`;
 
 const Spacer = styled.div`
-    ${CarretCss}
-`
+  ${CarretCss}
+`;
 
 const AccordionBody = styled.div`
-    border-radius: 10px;
-    padding-left: 10px;
-`
+  border-radius: 10px;
+  padding-left: 10px;
+`;
 
 const AccordionItem: FunctionComponent<AccordionMenuProps> = ({
-    option,
-    openMenu,
-    changeOpenMenu,
-    parentLevel,
-})=>{
-    const [openSubMenu, setopenSubMenu] = useState('')
-    const [showCarret, setshowCarret] = useState(true)
-    const currentLevel = parentLevel+1
+  option,
+  openMenu,
+  changeOpenMenu,
+  parentLevel,
+}) => {
+  const [openSubMenu, setopenSubMenu] = useState("");
+  const [showCarret, setshowCarret] = useState(true);
+  const currentLevel = parentLevel + 1;
 
-    function changeMenu(){
-        changeOpenMenu(option.uniqueKey)
+  function changeMenu() {
+    changeOpenMenu(option.uniqueKey);
 
-        if (isEmptyTitle(option)){
-            option.action && option.action()
-        }
+    if (isEmptyTitle(option)) {
+      option.action && option.action();
+    }
+  }
 
+  function changeOpenSubMenu(newMenu: string) {
+    if (openSubMenu == newMenu) {
+      setopenSubMenu("");
+      return;
     }
 
-    function changeOpenSubMenu(newMenu: string){
-        if (openSubMenu == newMenu) {
-            setopenSubMenu('')
-            return
-        }
+    setopenSubMenu(newMenu);
+  }
 
-        setopenSubMenu(newMenu)
-    }
-
-    const renderedOptions = useMemo(() => {
-        if (isSubAccordion(option)){
-            return option.content.map((value) => {
-                return (
-                    <AccordionItem 
-                        option={value}
-                        openMenu={openSubMenu}
-                        changeOpenMenu={changeOpenSubMenu}
-                        parentLevel={currentLevel}
-                    />
-                )
-            })
-        }
-
-        setshowCarret(false)
-
-        if (isAccordionItem(option)){
-            return (
-                <AccordionContent 
-                    key={option.uniqueKey}
-                    option={option.description}
-                    action={option.action}
-                />
-            )
-        }
-
+  const renderedOptions = useMemo(() => {
+    if (isSubAccordion(option)) {
+      return option.content.map((value) => {
         return (
-            <></>   
-        )
-    }, [openSubMenu])
-    
-    
+          <AccordionItem
+            key={`${option.uniqueKey}`}
+            option={value}
+            openMenu={openSubMenu}
+            changeOpenMenu={changeOpenSubMenu}
+            parentLevel={currentLevel}
+          />
+        );
+      });
+    }
 
-    return (
-        <AccordionContainer
-            className={`accordionContainer ${option.uniqueKey} ${option.commonClass || ""} container-lvl-${currentLevel}`}
+    setshowCarret(false);
+
+    if (isAccordionItem(option)) {
+      return (
+        <AccordionContent
+          key={option.uniqueKey}
+          option={option.description}
+          action={option.action}
+        />
+      );
+    }
+
+    return <></>;
+  }, [openSubMenu]);
+
+  return (
+    <AccordionContainer
+      className={`accordionContainer ${option.uniqueKey} ${
+        option.commonClass || ""
+      } container-lvl-${currentLevel}`}
+    >
+      <AccordionTitle
+        key={option.uniqueKey}
+        onClick={changeMenu}
+        className={`accordionTitle title-lvl-${currentLevel}`}
+      >
+        {option.title}
+        {showCarret ? (
+          <AccordionCarret />
+        ) : (
+          <Spacer className={`accordionSpacer`} />
+        )}
+      </AccordionTitle>
+      {openMenu == option.uniqueKey && (
+        <AccordionBody
+          className={`accordionBody body-lvl-${currentLevel} ${
+            option.bodyClass || ""
+          }`}
         >
-            <AccordionTitle 
-                key={option.uniqueKey}
-                onClick={changeMenu}
-                className={`accordionTitle title-lvl-${currentLevel}`}
-            >
-                {option.title}
-                {showCarret ?
-                    <AccordionCarret /> :
-                    <Spacer className={`accordionSpacer`}/>
-                }
-            </AccordionTitle>
-            {openMenu==option.uniqueKey && 
-                <AccordionBody className={`accordionBody body-lvl-${currentLevel} ${option.bodyClass || ""}`}>{
-                    renderedOptions}
-                </AccordionBody>
-            }
-        </AccordionContainer>
-    )
-}
+          {renderedOptions}
+        </AccordionBody>
+      )}
+    </AccordionContainer>
+  );
+};
 
-export default AccordionItem
+export default AccordionItem;
--- src/components/Dropdown/Hybrid/SubDropdown.tsx
+++ src/components/Dropdown/Hybrid/SubDropdown.tsx
@@ -1,81 +1,76 @@
-import { FunctionComponent, RefObject, useState } from 'react';
-import { DropdownMenuProps } from '../interface';
-import { 
-    ElemDropdownMenu,
-    ElemDropdownList,
-    ElemDropdownSubContainer,
-} from '../Elements'
-import { OutsideClickHandler } from '../../../hooks/OutsideClick';
-import { Carrets } from '../../../icons';
-import { renderBodyDropdown } from '../helper';
+import { FunctionComponent, RefObject, useState } from "react";
+import { DropdownMenuProps } from "../interface";
+import {
+  ElemDropdownMenu,
+  ElemDropdownList,
+  ElemDropdownSubContainer,
+} from "../Elements";
+import { OutsideClickHandler } from "../../../hooks/OutsideClick";
+import { Carrets } from "../../../icons";
+import { renderBodyDropdown } from "../helper";
 
 interface ClickDropdownMenuProps extends DropdownMenuProps {
-    parentRef?: RefObject<HTMLObjectElement>
-    closeCallback?: CallableFunction
+  parentRef?: RefObject<HTMLObjectElement>;
+  closeCallback?: CallableFunction;
 }
 
 export const SubDropdown: FunctionComponent<ClickDropdownMenuProps> = ({
-    label,
+  label,
+  options,
+  listOrientation,
+  parentRef,
+  showCarret,
+  closeCallback,
+}) => {
+  const [isOpen, setisOpen] = useState(false);
+
+  function toggleOpen() {
+    setisOpen(!isOpen);
+  }
+
+  function setClosed() {
+    setisOpen(false);
+  }
+
+  function setOpen() {
+    setisOpen(true);
+  }
+
+  parentRef && OutsideClickHandler(parentRef, setClosed);
+
+  const CarretNode = Carrets[listOrientation || "down"];
+  const body = renderBodyDropdown({
     options,
-    listOrientation,
-    parentRef,
-    showCarret,
-    closeCallback,
-})=>{
-    const [isOpen, setisOpen] = useState(false);
+    label,
+    SubDropdown,
+    closeCallback: closeCallback,
+  });
 
-    function toggleOpen(){
-        setisOpen(!isOpen)
-    }
+  return (
+    <ElemDropdownSubContainer mouseEnter={setOpen} mouseLeave={setClosed}>
+      <ElemDropdownMenu
+        onClick={toggleOpen}
+        elementRef={parentRef}
+        elementKey={`main-menu-${label}-${Math.random().toFixed(5)}`}
+      >
+        <div className="dropdown-menu-label">{label}</div>
+        {showCarret ? <CarretNode /> : <></>}
+      </ElemDropdownMenu>
 
-    function setClosed(){
-        setisOpen(false)
-    }
-
-    function setOpen(){
-        setisOpen(true)
-    }
-    
-    parentRef && OutsideClickHandler(parentRef, setClosed)
-
-    
-    const CarretNode = Carrets[listOrientation || "down"]
-    const body = renderBodyDropdown({
-        options,
-        label,
-        SubDropdown,
-        closeCallback: closeCallback
-    })
-
-    return(
-        <ElemDropdownSubContainer
-            mouseEnter={setOpen}
-            mouseLeave={setClosed}
-        >
-            <ElemDropdownMenu
-                onClick={toggleOpen}
-                elementRef={parentRef}
-                elementKey={`main-menu-${label}-${Math.random().toFixed(5)}`}
-            >
-                <div>{label}</div>    
-                {showCarret ? <CarretNode /> : <></>}
-            </ElemDropdownMenu>
-
-            { body.length && isOpen ?
-                <ElemDropdownList
-                    listOrientation={listOrientation}
-                >
-                    {body}
-                </ElemDropdownList>
-                : <></>
-            }
-        </ElemDropdownSubContainer>
-    )
-}
+      {body.length && isOpen ? (
+        <ElemDropdownList listOrientation={listOrientation}>
+          {body}
+        </ElemDropdownList>
+      ) : (
+        <></>
+      )}
+    </ElemDropdownSubContainer>
+  );
+};
 
 SubDropdown.defaultProps = {
-    listOrientation: "left",
-    showCarret: true,
-}
+  listOrientation: "left",
+  showCarret: true,
+};
 
-export default SubDropdown
+export default SubDropdown;
