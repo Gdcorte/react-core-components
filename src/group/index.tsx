dbc777f1ac4bcfad23ae34bff317f76640a8eec7
Gustavo Diniz da Corte <gdcorte@Gdcorte-M3-Mac.local>
2024-08-20 08:39:26 +0900

    expose group inputs

---
+++ src/group/index.tsx
@@ -0,0 +1,4 @@
+export { default as Checkbox } from "./Checkbox";
+export type { CheckboxProps } from "./Checkbox";
+export { default as Radiogroup } from "./RadioButton";
+export type { RadioButtonProps } from "./RadioButton";
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+export * from "./group";
 export * from "./hooks";
 export * from "./inputs";
 export * from "./validators";
