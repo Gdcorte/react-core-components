80ea0dcbb1dd33741dcd35e6234cacbbfa0b0d70
Gustavo Diniz da Corte <gdcorte@Gdcorte-M3-Mac.local>
2024-08-22 18:32:25 +0900

    timeline base def

--- src/timeline/Simple/index.tsx
+++ src/timeline/Simple/index.tsx
@@ -1,7 +1,25 @@
-export type SimpleTimelineProps = {};
+import { ReactNode } from "react";
+
+export type Activity = {
+  start: Date;
+  icon?: ReactNode;
+  title: ReactNode;
+  place: string;
+  color?: string;
+};
+
+export type SimpleTimelineProps = {
+  customColor?: string;
+  activities: Activity[];
+};
 
 type Props = {} & SimpleTimelineProps;
 
 export default function SimpleTimeline({ ...props }: Props) {
-  return <>Timeline Woohoo</>;
+  return (
+    <>
+      Timeline Woohoo
+      <pre>{JSON.stringify(props, undefined, 2)}</pre>
+    </>
+  );
 }
