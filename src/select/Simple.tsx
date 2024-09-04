ff6498198ccf0e10fa8d6d510877e7655f0367b5
Gustavo Diniz da Corte <gdcorte@Gdcorte-M3-Mac.local>
2024-09-05 01:18:54 +0900

    style select to make it more minimalistic

--- src/select/Simple.tsx
+++ src/select/Simple.tsx
@@ -2,19 +2,79 @@ import Select, { GroupBase, Props } from "react-select";
 import styled from "styled-components";
 import { SelectOption } from "./interface";
 
-const StyledSelect = styled(Select)``;
+const StyledSelect = styled(Select)<{
+  $separator: boolean;
+}>`
+  background: inherit;
+
+  .myselect__control {
+    min-height: unset;
+
+    border: inherit;
+    background: inherit;
+
+    text-decoration: none;
+
+    cursor: pointer;
+  }
+
+  .myselect__control.myselect__control--is-focused {
+    border: inherit;
+    box-shadow: none;
+  }
+
+  .myselect__value-container {
+    padding: 2px;
+  }
+
+  .myselect__input-container {
+    margin: 0;
+  }
+
+  .myselect__indicator-separator {
+    background: transparent;
+  }
+  .myselect__indicator {
+    padding: 0;
+    border-left: ${({ $separator }) => ($separator ? "1px solid;" : "none")};
+  }
+
+  .myselect__menu {
+    background: inherit;
+  }
+
+  .myselect__option {
+    cursor: pointer;
+  }
+
+  .myselect__option--is-focused {
+    background: ${({ theme }) => theme.background.contrast};
+  }
+`;
 
 type CustomProps<
   Option = SelectOption,
   IsMulti extends boolean = false,
   Group extends GroupBase<Option> = GroupBase<Option>,
-> = Props<Option, IsMulti, Group> & {};
+> = Props<Option, IsMulti, Group> & {
+  separator?: boolean;
+};
 
 export default function SimpleSelect<
   Option = SelectOption,
   IsMulti extends boolean = false,
   Group extends GroupBase<Option> = GroupBase<Option>,
->({ ...props }: CustomProps<Option, IsMulti, Group>) {
-  // @ts-expect-error ongoing issue with Option generics and typescripts
-  return <StyledSelect {...props} />;
+>({
+  classNamePrefix,
+  separator = false,
+  ...props
+}: CustomProps<Option, IsMulti, Group>) {
+  return (
+    // @ts-expect-error ongoing issue with Option generics and typescripts
+    <StyledSelect
+      $separator={separator}
+      {...props}
+      classNamePrefix="myselect"
+    />
+  );
 }
