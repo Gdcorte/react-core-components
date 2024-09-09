1f7d5e3d3fa25d75ad5376089680e080e0877e49
Gustavo Diniz da Corte <gdcorte@Gdcorte-M3-Mac.local>
2024-09-09 18:48:05 +0900

    expose button props

--- src/button/index.tsx
+++ src/button/index.tsx
@@ -1,3 +1,8 @@
 export { default as OutlineButton } from "./Outline";
+export type { OutlineButtonProps } from "./Outline";
+
 export { default as SolidButton } from "./Solid";
+export type { SolidButtonProps } from "./Solid";
+
 export { default as StaticButton } from "./Transparent";
+export type { TransparentButtonProps as StaticButtonProps } from "./Transparent";
